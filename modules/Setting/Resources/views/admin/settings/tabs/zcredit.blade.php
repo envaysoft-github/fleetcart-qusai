b50545106ca9dc28bf0e34be9f8dd0d208d703c2
Tohidul Islam <raptor.dnj@gmail.com>
2025-08-06 20:26:54 +0600

    various setting in payment setting for zcredit

--- modules/Setting/Resources/views/admin/settings/tabs/zcredit.blade.php
+++ modules/Setting/Resources/views/admin/settings/tabs/zcredit.blade.php
@@ -4,5 +4,37 @@
         {{ Form::text('translatable[zcredit_label]', trans('setting::attributes.translatable.zcredit_label'), $errors, $settings, ['required' => true]) }}
         {{ Form::textarea('translatable[zcredit_description]', trans('setting::attributes.translatable.zcredit_description'), $errors, $settings, ['rows' => 3, 'required' => true]) }}
         {{ Form::textarea('zcredit_key', trans('setting::attributes.zcredit_key'), $errors, $settings, ['rows' => 3, 'required' => true]) }}
+        {{ Form::select('zcredit_holderid', trans('setting::attributes.zcredit_holderid'), $errors, $options, $settings, ['required' => true]) }}
+        {{ Form::select('zcredit_holder_phone', trans('setting::attributes.zcredit_holder_phone'), $errors, $options, $settings, ['required' => true]) }}
+        {{ Form::select('zcredit_holder_email', trans('setting::attributes.zcredit_holder_email'), $errors, $options, $settings, ['required' => true]) }}
+        {{ Form::select('zcredit_installment_option', trans('setting::attributes.zcredit_installment_option'), $errors, $installment_options, $settings, ['required' => true]) }}
+        {{ Form::number(
+            'zcredit_installment_min',
+            trans('setting::attributes.zcredit_installment_min'),
+            $errors,
+            $settings,
+            [
+                'required' => true,
+                'min' => 1,
+                'max' => 12,
+                'step' => 1,
+                'value' => old('zcredit_installment_min', 0)
+            ]
+        ) }}
+
+        {{ Form::number(
+    'zcredit_installment_max',
+    trans('setting::attributes.zcredit_installment_max'),
+    $errors,
+    $settings,
+    [
+        'required' => true,
+        'min' => 1,
+        'max' => 12,
+        'step' => 1,
+        'value' => old('zcredit_installment_max', 0)
+    ]
+) }}
+
     </div>
 </div>
